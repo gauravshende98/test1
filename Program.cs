9d66f734454e3cfc982f5e4967aed6cf0c62cd7e
gauravshende98 <gauravshende22@gmail.com>
2022-10-14 13:51:27 +0530

    hlw this is new

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharp_new_practice6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            //Console.WriteLine(i++);
+            //Console.WriteLine(i++);
+            //Console.WriteLine(i++);
+            //Console.WriteLine(i++);
+           
+
+            i =i++ + i++ + i++ + i;
+            Console.WriteLine(i++ + i);
+            Console.ReadLine();
+        }
+    }
+}
